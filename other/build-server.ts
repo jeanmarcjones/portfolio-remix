e86f542f99ea3ea8f484394a24ea4c5bbfdc7480
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2024-08-10 10:12:59 +0100

    refactor: Simplify build script

--- other/build-server.ts
+++ other/build-server.ts
@@ -3,41 +3,17 @@ import { fileURLToPath } from 'node:url'
 
 import esbuild from 'esbuild'
 import fsExtra from 'fs-extra'
-import { globSync } from 'glob'
 
 const pkg = fsExtra.readJsonSync(path.join(process.cwd(), 'package.json'))
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const here = (...s: Array<string>) => path.join(__dirname, ...s)
-const globsafe = (s: string) => s.replaceAll('\\', '/')
 
-const allFiles = globSync(globsafe(here('../server/**/*.*')), {
-  ignore: [
-    // 'server/dev-server.js', // for development only
-    '**/tsconfig.json',
-    '**/eslint*',
-    '**/__tests__/**',
-  ],
-})
-
-const entries = []
-for (const file of allFiles) {
-  if (/\.(ts|js|tsx|jsx)$/.test(file)) {
-    entries.push(file)
-  } else {
-    const dest = file.replace(here('../server'), here('../server-build'))
-    fsExtra.ensureDirSync(path.parse(dest).dir)
-    fsExtra.copySync(file, dest)
-    console.log(`copied: ${file.replace(`${here('../server')}/`, '')}`)
-  }
-}
-
-console.log()
 console.log('building...')
 
-esbuild
-  .build({
-    entryPoints: entries,
+try {
+  await esbuild.build({
+    entryPoints: [here('../server/index.ts')],
     outdir: here('../server-build'),
     target: [`node${pkg.engines.node}`],
     platform: 'node',
@@ -45,9 +21,8 @@ esbuild
     format: 'esm',
     logLevel: 'info',
   })
-  // eslint-disable-next-line unicorn/prefer-top-level-await -- This is fine in a cli script
-  .catch((error: unknown) => {
-    console.error(error)
-    // eslint-disable-next-line unicorn/no-process-exit -- This is a cli script
-    process.exit(1)
-  })
+} catch (error: unknown) {
+  console.error(error)
+  // eslint-disable-next-line unicorn/no-process-exit -- This is a cli script
+  process.exit(1)
+}
