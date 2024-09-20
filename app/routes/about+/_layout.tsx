25df4e178401b025f176f23282428596e975859d
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2024-09-20 14:04:45 +0100

    feat: Created copy for about page

--- app/routes/about+/_layout.tsx
+++ app/routes/about+/_layout.tsx
@@ -5,12 +5,13 @@ import { Separator } from '~/components/ui/separator'
 export default function AboutLayout() {
   return (
     <div className="container">
-      <h1 className="h0 mb-1 ms-1">About</h1>
-      <Separator className="mb-4" />
+      <h1 className="h0 mb-2 ms-1">About</h1>
+      <Separator className="mb-8" />
 
       <div className="flex flex-row gap-14">
         <div className="flex min-w-max flex-col items-center gap-1">
           <p className="text-3xl font-bold">Jean-Marc Jones</p>
+
           <p className="text-indigo-300">React Native Developer</p>
         </div>
 
